135fcfb6412f01d1b89850c0d958be1e9991c4a7
Maxoduke <maxoduke@outlook.com>
2023-10-14 06:08:09 +0530

    Switch to release channel with some code improvements

--- src/main/java/dev/maxoduke/mods/portallinkingcompass/PortalLinkingCompass.java
+++ src/main/java/dev/maxoduke/mods/portallinkingcompass/PortalLinkingCompass.java
@@ -1,10 +1,10 @@
-package me.maxoduke.mod.portallinkingcompass.module;
+package dev.maxoduke.mods.portallinkingcompass;
 
-import me.maxoduke.mod.portallinkingcompass.PortalLinkingCompassMod;
-import me.maxoduke.mod.portallinkingcompass.item.PortalLinkingCompassItem;
-
+import dev.maxoduke.mods.portallinkingcompass.item.PortalLinkingCompassItem;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.client.renderer.item.CompassItemPropertyFunction;
@@ -15,23 +15,34 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class PortalLinkingCompass
+@SuppressWarnings("SpellCheckingInspection")
+public class PortalLinkingCompass implements ModInitializer, ClientModInitializer
 {
+    public static final String MOD_ID = "portallinkingcompass";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     public static final String ITEM_NAME = "portal_linking_compass";
     public static final Item ITEM = new PortalLinkingCompassItem(new FabricItemSettings());
-    public static final ResourceLocation SOUND_ID = new ResourceLocation(PortalLinkingCompassMod.MOD_ID, "item.portal_linking_compass.lock");
+    public static final ResourceLocation SOUND_ID = new ResourceLocation(MOD_ID, "item.portal_linking_compass.lock");
     public static final SoundEvent SOUND_EVENT = SoundEvent.createVariableRangeEvent(SOUND_ID);
 
-    public static void init()
+    @Override
+    public void onInitialize()
     {
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.TOOLS_AND_UTILITIES).register((content) -> content.accept(ITEM));
+
         Registry.register(BuiltInRegistries.SOUND_EVENT, SOUND_ID, SOUND_EVENT);
-        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(PortalLinkingCompassMod.MOD_ID, ITEM_NAME), ITEM);
+        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(MOD_ID, ITEM_NAME), ITEM);
+
+        LOGGER.info("Portal Linking Compass initialized!");
     }
 
+    @Override
     @Environment(EnvType.CLIENT)
-    public static void initClient()
+    public void onInitializeClient()
     {
         ItemProperties.register(ITEM, new ResourceLocation("angle"), new CompassItemPropertyFunction(PortalLinkingCompassItem::pointToTarget));
     }
