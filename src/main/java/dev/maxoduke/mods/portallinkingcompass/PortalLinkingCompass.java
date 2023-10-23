89f407f5007c0ae40a4fae38638ae00c2a5e0f57
Maxoduke <maxoduke@outlook.com>
2023-10-23 12:06:17 +0530

    Port to Forge

---
+++ src/main/java/dev/maxoduke/mods/portallinkingcompass/PortalLinkingCompass.java
@@ -0,0 +1,68 @@
+package dev.maxoduke.mods.portallinkingcompass;
+
+import dev.maxoduke.mods.portallinkingcompass.item.PortalLinkingCompassItem;
+import net.minecraft.client.renderer.item.CompassItemPropertyFunction;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("SpellCheckingInspection")
+@Mod(PortalLinkingCompass.MOD_ID)
+public class PortalLinkingCompass
+{
+    public static final String MOD_ID = "portallinkingcompass";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    private static final String ITEM_NAME = "portal_linking_compass";
+    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+    public static final RegistryObject<Item> ITEM = ITEMS.register(ITEM_NAME, () -> new PortalLinkingCompassItem(new Item.Properties()));
+
+    private static final String SOUND_NAME = "item.portal_linking_compass.lock";
+    private static final ResourceLocation SOUND_ID = new ResourceLocation(PortalLinkingCompass.MOD_ID, SOUND_NAME);
+    private static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MOD_ID);
+    public static final RegistryObject<SoundEvent> SOUND_EVENT = SOUND_EVENTS.register(SOUND_NAME, () -> SoundEvent.createVariableRangeEvent(SOUND_ID));
+
+    public PortalLinkingCompass()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ITEMS.register(modEventBus);
+        SOUND_EVENTS.register(modEventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+
+        LOGGER.info("Portal Linking Compass initialized!");
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES)
+            event.accept(ITEM);
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            ItemProperties.register(ITEM.get(), new ResourceLocation("angle"), new CompassItemPropertyFunction(PortalLinkingCompassItem::pointToTarget));
+        }
+    }
+}
